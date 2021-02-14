10e5b604bde5698048883c444e6a78d4134d7491
Almanac <ruoyw0@gmail.com>
2021-02-14 15:24:34 -0800

    Added in Ship class to have some basic functionality such as holding the coordinates and for checking if it has been sunk. Added ships to GameState. Looking into implementing MakeMove function next.

--- GameState.cs
+++ GameState.cs
@@ -10,5 +10,34 @@ namespace BattleshipProject
     // Passed around to be updated by other classes
     class GameState
     {
+        // String representation of hits, misses, and ships
+        string boardState;
+        List<Ship> ships;
+        int numShots;
+
+        // check if game can continue
+        public bool MovesAvailable()
+        {
+            return numShots < 100;
+        }
+
+        // check if this player has won (sunk all of opponent's ships)
+        public bool AllShipsSunk()
+        {
+            bool result = true;
+            foreach (Ship s in ships)
+            {
+                if (s.IsAlive())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool MakeMove()
+        {
+            return true;
+        }
     }
 }
---
+++ Ship.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipProject
+{
+    class Ship
+    {
+        string shipName;
+        int shipSize;
+        HashSet<Tuple<int, int>> shipCoordinates;
+        int hits;
+
+        // It is the responsibility of the module creaing the ship to do
+        // handle rotation and confirming that the ship location is legal
+        public Ship(string name, Tuple<int,int>[] coordinates)
+        {
+            shipName = name;
+            shipSize = coordinates.Length;
+            hits = 0;
+            for (int i = 0; i < coordinates.Length; i++) {
+                shipCoordinates.Add(coordinates[i]);
+            }
+        }
+        public bool IsAlive()
+        {
+            return hits == shipSize;
+        }
+
+        // It is the responsibility of the module calling this to ensure
+        // That the provided value is not a duplicate.
+        public bool hitShip(int x, int y)
+        {
+            Tuple<int, int> temp = new Tuple<int, int>(x, y);
+            if (shipCoordinates.Contains(temp))
+            {
+                hits += 1;
+                return true;
+            }
+            return false;
+        }
+
+        public string ShipName()
+        {
+            return shipName;
+        }
+    }
+}
