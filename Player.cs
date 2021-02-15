2a6ae6e4e930687da58f4a5fd724978dcf2e389d
Almanac <ruoyw0@gmail.com>
2021-02-15 13:19:10 -0800

    Added in disclaimer into every file to emphasize that this is purely for educational purposes

--- Player.cs
+++ Player.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ * Disclaimer: I do not own the rights to Battleship.
+ * This is purely a project for educational purposes in learning and practicing programming
+ **/
 namespace BattleshipProject
 {
     // Made the decision to have IO handling inside of player class
